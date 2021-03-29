57df6fd9a3f72b4037a0450aabdcb8913f561c20
Shun Yabutani <71088052+syabutan@users.noreply.github.com>
2021-03-29 14:42:26 -0600

    js and model

---
+++ EmbarkToTalk/src/app/models/solution.ts
@@ -0,0 +1,12 @@
+
+export interface Solution {
+  firstChoice: string;
+  secondChoice: string;
+  userText: string;
+  computerText: string;
+}
+
+export interface GoogleObj {
+  q: string[];
+  target: string;
+}
